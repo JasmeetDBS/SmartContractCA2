eaa4bc73489d82cdc5b1ced92de694afefe451f2
JasmeetDBS <96395467+JasmeetDBS@users.noreply.github.com>
2021-12-20 21:29:13 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -56,6 +56,7 @@ tx_dict = Greeter.constructor().buildTransaction({
 signed_txn = W3.eth.account.sign_transaction(tx_dict, private_key=privateKey)
 #diagnostics
 #print(signed_txn)
+print(privateKey)
 print("Deploying the Smart Contract")
 result = W3.eth.sendRawTransaction(signed_txn.rawTransaction)
 #diagnostics
